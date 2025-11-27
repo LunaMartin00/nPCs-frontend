5e98db93cef8a08a929ed4fe100c9a692026bcbc
CarlosCruz99 <00013324@uca.edu.sv>
2025-11-27 09:34:48 -0600

    fix: Arreglar rutas de categoría

--- nPCs-proyect/src/components/clientComponents/ClientNavbar.jsx
+++ nPCs-proyect/src/components/clientComponents/ClientNavbar.jsx
@@ -85,9 +85,9 @@ function ClientNavbar(args) {
               Explorar Productos
             </DropdownToggle>
             <DropdownMenu>
-              <DropdownItem tag={Link} to="/cliente/explorar/procesadores">Procesadores</DropdownItem>
+              <DropdownItem tag={Link} to="/cliente/explorar/cpu">Procesadores</DropdownItem>
               <DropdownItem tag={Link} to="/cliente/explorar/placas-madre">Motherboards</DropdownItem>
-              <DropdownItem tag={Link} to="/cliente/explorar/tarjetas-graficas">Tarjetas de video</DropdownItem>
+              <DropdownItem tag={Link} to="/cliente/explorar/gpu">Tarjetas de video</DropdownItem>
               <DropdownItem tag={Link} to="/cliente/explorar/memorias-ram">Memorias RAM</DropdownItem>
               <DropdownItem tag={Link} to="/cliente/explorar/almacenamiento">Almacenamiento</DropdownItem>
               <DropdownItem tag={Link} to="/cliente/explorar/fuentes-poder">Fuentes de Poder</DropdownItem>
--- nPCs-proyect/src/components/storeComponents/StoreNavbar.jsx
+++ nPCs-proyect/src/components/storeComponents/StoreNavbar.jsx
@@ -85,9 +85,9 @@ function ClientNavbar(args) {
               Explorar Productos
             </DropdownToggle>
             <DropdownMenu>
-              <DropdownItem tag={Link} to="/tienda/explorar/procesadores">Procesadores</DropdownItem>
+              <DropdownItem tag={Link} to="/tienda/explorar/cpu">Procesadores</DropdownItem>
               <DropdownItem tag={Link} to="/tienda/explorar/placas-madre">Motherboards</DropdownItem>
-              <DropdownItem tag={Link} to="/tienda/explorar/tarjetas-graficas">Tarjetas de video</DropdownItem>
+              <DropdownItem tag={Link} to="/tienda/explorar/gpu">Tarjetas de video</DropdownItem>
               <DropdownItem tag={Link} to="/tienda/explorar/memorias-ram">Memorias RAM</DropdownItem>
               <DropdownItem tag={Link} to="/tienda/explorar/almacenamiento">Almacenamiento</DropdownItem>
               <DropdownItem tag={Link} to="/tienda/explorar/fuentes-poder">Fuentes de Poder</DropdownItem>
